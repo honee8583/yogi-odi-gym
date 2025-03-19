92779504924e4d3f1038d929fb90117b10c37b14
leedahoon <ekgns0508@gmail.com>
2025-03-19 15:25:04 +0900

    refactor: 게시글을 사용한 댓글 개수 조회 쿼리메서드 수정

--- src/main/java/com/health/yogiodigym/board/repository/CommentRepository.java
+++ src/main/java/com/health/yogiodigym/board/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.health.yogiodigym.board.repository;
 
+import com.health.yogiodigym.board.entity.Board;
 import com.health.yogiodigym.board.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByBoardIdOrderByCreateDateTimeAsc(Long boardId);
 
-    Long countByBoardId(Long boardId);
+    Long countByBoard(Board board);
 }

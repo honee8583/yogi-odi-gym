b65e1dcbce1f16069267d6519450cd4b00534777
leedahoon <ekgns0508@gmail.com>
2025-03-19 15:39:06 +0900

    refactor: 게시글 컨트롤러 수정
    - Redirect 모두 제거
    - 파라미터 전달 -> DTO 사용
    - 게시글 수정 메서드 POST -> PUT 변경

--- src/main/java/com/health/yogiodigym/board/controller/BoardController.java
+++ src/main/java/com/health/yogiodigym/board/controller/BoardController.java
@@ -1,69 +1,59 @@
 package com.health.yogiodigym.board.controller;
 
-import com.health.yogiodigym.admin.service.service.AdminBoardService;
-import com.health.yogiodigym.board.dto.BoardDto.*;
+import static com.health.yogiodigym.common.message.SuccessMessage.REGISTER_BOARD_SUCCESS;
+import static com.health.yogiodigym.common.message.SuccessMessage.SEARCH_BOARD_SUCCESS;
+import static com.health.yogiodigym.common.message.SuccessMessage.UPDATE_BOARD_SUCCESS;
+
+import com.health.yogiodigym.board.dto.BoardDto.BoardDetailDto;
+import com.health.yogiodigym.board.dto.BoardDto.BoardSearchRequestDto;
+import com.health.yogiodigym.board.dto.BoardDto.BoardWriteRequestDto;
 import com.health.yogiodigym.board.service.BoardService;
+import com.health.yogiodigym.common.dto.PageResponseDto;
 import com.health.yogiodigym.common.response.HttpResponse;
-import com.health.yogiodigym.lesson.dto.LessonDto;
-import com.health.yogiodigym.member.entity.Member;
 import com.health.yogiodigym.member.entity.MemberOAuth2User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
-
-import static com.health.yogiodigym.common.message.SuccessMessage.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
-    private final AdminBoardService adminBoardService;
 
     @GetMapping("/search")
-    public ResponseEntity<?> searchBoard(@RequestParam(required = false) String boardKeyword,
-                                         @RequestParam(required = false) String searchColumn,
-                                         @RequestParam(required = false) List<Long> categories,
-                                         @RequestParam(defaultValue = "0") int page,
-                                         @RequestParam(defaultValue = "10") int size) {
-
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
-        Page<BoardDetailDto> boards = boardService.searchBoards(boardKeyword, searchColumn, categories, pageable);
-
+    public ResponseEntity<?> searchBoard(BoardSearchRequestDto searchRequest,
+                                         @PageableDefault Pageable pageable) {
+        log.info("게시글 검색 : {}", searchRequest);
+        PageResponseDto<BoardDetailDto> boards = boardService.searchBoards(searchRequest, pageable);
         return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, SEARCH_BOARD_SUCCESS.getMessage(), boards));
     }
 
-    @PostMapping("/register")
-    public RedirectView registerBoard(@ModelAttribute BoardRequestDto dto,
-                                      @AuthenticationPrincipal MemberOAuth2User loginUser) {
-        Member loginMember = loginUser.getMember();
-        boardService.registerBoard(dto, loginMember);
-
-        return new RedirectView("/board");
+    @PostMapping
+    public ResponseEntity<?> registerBoard(@AuthenticationPrincipal MemberOAuth2User loginUser,
+                                           @RequestBody BoardWriteRequestDto registerRequest) {
+        log.info("게시글 작성 : {}", registerRequest);
+        boardService.registerBoard(registerRequest, loginUser.getMember());
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, REGISTER_BOARD_SUCCESS.getMessage(), null));
     }
 
-    @PostMapping("/edit")
-    public RedirectView editBoard(@ModelAttribute BoardDetailDto dto) {
-        boardService.editBoard(dto);
-        return new RedirectView("/board/" + dto.getId());
-    }
-
-    @PostMapping("/delete")
-    public ResponseEntity<?> adminDeleteBoard(@RequestBody List<Long> ids) {
-
-        adminBoardService.deleteAllById(ids);
-        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, ADMIN_BOARD_DELETE_SUCCESS.getMessage(), null));
-
+    @PutMapping
+    public ResponseEntity<?> editBoard(@AuthenticationPrincipal MemberOAuth2User loginUser,
+                                       @RequestBody BoardWriteRequestDto updateRequest) {
+        log.info("게시글 수정 : {}", updateRequest);
+        boardService.editBoard(loginUser.getMember(), updateRequest);
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, UPDATE_BOARD_SUCCESS.getMessage(), null));
     }
 }
